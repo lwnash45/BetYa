4e3aabbd8b90fcd7a6dabe1844d17a966616d38a
LoganNash <lwnash45@gmail.com>
2018-11-02 14:12:04 -0700

    spec

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
-
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.render('../index');
-// });
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+});
 
 server.listen(3000, () => {
     console.log(`Server is up on port ${port}!!`);
